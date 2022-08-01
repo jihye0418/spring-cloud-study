9f5290d27c41dbe7016a6863c0396fddbcf49cff
kjh <lykim0418@naver.com>
2022-08-01 18:59:43 +0900

    Use Eureka Server

--- first-service/src/main/java/com/example/secondservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/secondservice/FirstServiceController.java
@@ -1,15 +1,24 @@
 package com.example.secondservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+    //환경설정
+    Environment env;
+
+    public FirstServiceController(Environment env){
+        this.env = env;
+    }
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the Second Service";
@@ -22,7 +31,10 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from First Service";
+    public String check(HttpServletRequest request){
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("Hi, there. This is a message from First Service on PORT %s"
+        , env.getProperty("local.server.port"));
     }
 }
