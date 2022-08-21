5b3569915425d6cffa6fd1cf98c6f0f0b1cc9147
kjh <lykim0418@naver.com>
2022-08-21 22:18:56 +0900

    ADD AuthenticationFilter

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -5,6 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -12,8 +14,20 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
-
+        //http.authorizeRequests().antMatchers("/users/**").permitAll();
+        //모든 사용자에 대한 권한 부여x
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress(" 허용할 IP주소 ")
+                .and()
+                .addFilter(getAuthenticationFilter());
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception{
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        //작업할 수 있는 Manger 추가
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
